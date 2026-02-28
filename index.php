7fd0ae57522514e2e6a19a1092dad166020a6c06
Maged Groups <magedyaseengroups@gmail.com>
2026-02-28 21:37:58 +0200

    Loops(for, while-do, do-while)

--- index.php
+++ index.php
@@ -7,4 +7,6 @@ require_once './App/Helpers/functions.php';
 // App\Http\Controllers\ProductController::index();
 // App\Http\Controllers\ProductController::refine();
 
-App\Http\Controllers\UserController::old_users();
+// App\Http\Controllers\UserController::old_users();
+
+
