670744558085c25749068d16cbe7c461f887bd6e
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-03-18 10:57:57 +0200

    index page for AI created

---
+++ aly/ai/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AI Page</title>
+</head>
+
+<body>
+    <h1>AI is fun</h1>
+
+</body>
+
+</html>
